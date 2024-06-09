8ddc63a6dbd3a0d5f8f5919fb47f7cc02a531838
Thenura Dulnath <74284748+Thenuradulnath@users.noreply.github.com>
2024-06-10 06:33:28 +1000

    Add files via upload

---
+++ Thing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemesterTest
+{
+    public abstract class Thing
+    {
+        private string _number;
+        private string _name;
+        public Thing(string number, string name)
+        {
+            _number = number;
+            _name = name;
+        }
+        public abstract void Print();
+        public abstract decimal Total();
+        
+        public String Number
+        {
+            get { return _number; }
+        }
+        public String Name
+        {
+            get { return _name; }
+        }
+    }
+}
